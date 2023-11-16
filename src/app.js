7c3cddb5c5263292cdc8cda4e96a74fb95788217
BachLe27 <bachlamsung123@gmail.com>
2023-11-16 15:02:33 +0700

    Initialize Server

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const compression = require('compression');
+const express = require('express');
+const { default: helmet } = require('helmet');
+const morgan = require('morgan');
+const app = express();
+
+// init middleware
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+// init db
+
+// init routers
+app.get('/', (req, res, next) => {
+
+  const strCompress = 'Hello BachLe'
+
+  return res.status(200).json({
+    message: "OK",
+    metadata: strCompress.repeat(10000)
+  })
+})
+// handle error
+
+
+module.exports = app
